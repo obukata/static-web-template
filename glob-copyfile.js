c37a12a7747d936d8345ddd342feae2206445c4b
obukata <obukata@mj-star.jp>
2023-06-19 13:19:51 +0900

    glob-copyfile.js にてパターンと除外ファイルを設定。コピー時にファイル名もディレクトリ扱いされる問題

--- glob-copyfile.js
+++ glob-copyfile.js
@@ -1,18 +1,33 @@
-import glob from 'glob'
+import { glob, globSync, globStream, globStreamSync, Glob } from 'glob'
 import cpx from 'cpx'
 
-const sourcePattern = 'src/!(_*)/!(_*)'
 const destinationDirectory = 'dist'
 
-glob(sourcePattern, {}, (err, files) => {
-  if (err) {
-    console.error('Error in glob:', err)
-    process.exit(1)
-  }
-
-  files.forEach((file) => {
-    const destination = file.replace('src', destinationDirectory)
-    cpx.copySync(file, destination)
-    console.log(`Copied ${file} to ${destination}`)
-  })
+const matchPattern = globSync('src/**/*', {
+  ignore: [
+    '**/_*',
+    '**/_*/**'
+  ]
 })
+
+matchPattern.forEach(path => {
+  const destination = path.replace('src', destinationDirectory)
+  console.log(destination)
+  cpx.copySync(path, destination)
+  console.log(`Copied ${path} to ${destination}`)
+})
+// console.log(matchPattern)
+
+
+// glob(sourcePattern, {}, (err, files) => {
+//   if (err) {
+//     console.error('Error in glob:', err)
+//     process.exit(1)
+//   }
+
+//   files.forEach((file) => {
+//     const destination = file.replace('src', destinationDirectory)
+//     cpx.copySync(file, destination)
+//     console.log(`Copied ${file} to ${destination}`)
+//   })
+// })
