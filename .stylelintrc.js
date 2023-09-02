f288012eb53c7b89c60db048e4e3cd1fac45256b
obukata <obukata@mj-star.jp>
2023-09-02 12:20:48 +0900

    stylelintルール更新、package.jsonタスク整理

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -10,13 +10,13 @@ module.exports = {
     "selector-id-pattern": null,
     "selector-class-pattern": null,
     "keyframes-name-pattern": null,
-    "function-url-quotes": ["always", { "severity": "warning" }],
-    "number-max-precision": [3, { "severity": "warning" }],
-    "alpha-value-notation": ["number", { "severity": "warning" }],
-    "font-family-name-quotes": ["always-where-recommended", { "severity": "warning" }],
-    "property-no-vendor-prefix": [true, { "ignoreProperties": ["appearance", "text-size-adjust"] }],
+    "function-url-quotes": null,
+    "number-max-precision": null,
+    "alpha-value-notation": null,
+    "font-family-name-quotes": null,
     "no-descending-specificity": null,
     "no-duplicate-selectors": null,
     "declaration-block-no-duplicate-custom-properties": null,
+    "property-no-vendor-prefix": null,
   }
 }
